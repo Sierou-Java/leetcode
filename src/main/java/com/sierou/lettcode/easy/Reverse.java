1f7a8b453142c2fcb434f96fce774fcace9eb52c
Sierou-Java <sierou.wang.hongyue@gmail.com>
2019-10-28 21:28:51 +0800

    整数反转

--- src/main/java/com/sierou/lettcode/easy/Reverse.java
+++ src/main/java/com/sierou/lettcode/easy/Reverse.java
@@ -25,7 +25,7 @@ import java.beans.Transient;
  * 链接：https://leetcode-cn.com/problems/reverse-integer
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Reverse {
+    public class Reverse {
 
     public static int reverse(int x) {
         int rev = 0;
