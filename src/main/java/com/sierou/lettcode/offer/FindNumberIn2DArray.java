11af8c44070be16e3f18b4567e0b94f2fccec621
Sierou-Java <javayue@yeah.net>
2020-07-17 09:49:22 +0800

    offer

---
+++ src/main/java/com/sierou/lettcode/offer/FindNumberIn2DArray.java
@@ -0,0 +1,57 @@
+package com.sierou.lettcode.offer;
+
+/**
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * <p>
+ * 示例:
+ * <p>
+ * 现有矩阵 matrix 如下：
+ * <p>
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ * <p>
+ * 给定 target = 20，返回 false。
+ * <p>
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= n <= 1000
+ * <p>
+ * 0 <= m <= 1000
+ * <p>
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author wanghongyue
+ * 2020-07-17 9:04 上午
+ **/
+public class FindNumberIn2DArray {
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+
+        solution(matrix, 11);
+    }
+
+
+    public static boolean solution(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (target == matrix[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
